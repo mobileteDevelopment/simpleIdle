038df400edf7c47dabfff0a6458a808052301571
mobileteDevelopment <72264817+mobileteDevelopment@users.noreply.github.com>
2020-10-02 09:26:18 -0500

    Create IdlePass.cs

---
+++ IdlePass.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using System.Collections;
+
+public class IdlePass : MonoBehaviour {
+
+    //VARIABLES
+    
+    public double premiumPassActivationValue;
+    public double premiumPassCurrentTier;
+    public double premiumPassTierCap;
+    
+    public double freePassCurrentTier;
+    public double freePassTierCap;
+    
+    //ALL TIER GAME OBJECTS
+    public GameObject premiumPassTier1;
+    public GameObject premiumPassTier2;
+    public GameObject premiumPassTier3;
+    public GameObject premiumPassTier4;
+    public GameObject premiumPassTier5;
+    public GameObject premiumPassTier6;
+    public GameObject premiumPassTier7;
+    public GameObject premiumPassTier8;
+    public GameObject premiumPassTier9;
+    public GameObject premiumPassTier10;
+    public GameObject premiumPassTier11;
+    public GameObject premiumPassTier12;
+    public GameObject premiumPassTier13;
+    public GameObject premiumPassTier14;
+    public GameObject premiumPassTier15;
+    public GameObject premiumPassTier16;
+    public GameObject premiumPassTier17;
+    public GameObject premiumPassTier18;
+    public GameObject premiumPassTier19;
+    public GameObject premiumPassTier20;
+    public GameObject premiumPassTier21;
+    public GameObject premiumPassTier22;
+    public GameObject premiumPassTier23;
+    public GameObject premiumPassTier24;
+    public GameObject premiumPassTier25;
+    public GameObject premiumPassTier26;
+    public GameObject premiumPassTier27;
+    public GameObject premiumPassTier28;
+    public GameObject premiumPassTier29;
+    public GameObject premiumPassTier30;
+    public GameObject premiumPassTier31;
+    public GameObject premiumPassTier32;
+    public GameObject premiumPassTier33;
+    public GameObject premiumPassTier34;
+    public GameObject premiumPassTier35;
+    public GameObject premiumPassTier36;
+    public GameObject premiumPassTier37;
+    public GameObject premiumPassTier38;
+    public GameObject premiumPassTier39;
+    public GameObject premiumPassTier40;
+    public GameObject premiumPassTier41;
+    public GameObject premiumPassTier42;
+    public GameObject premiumPassTier43;
+    public GameObject premiumPassTier44;
+    public GameObject premiumPassTier45;
+    public GameObject premiumPassTier46;
+    public GameObject premiumPassTier47;
+    public GameObject premiumPassTier48;
+    public GameObject premiumPassTier49;
+    public GameObject premiumPassTier50;
+    public GameObject premiumPassTier51;
+    public GameObject premiumPassTier52;
+    public GameObject premiumPassTier53;
+    public GameObject premiumPassTier54;
+    public GameObject premiumPassTier55;
+    public GameObject premiumPassTier56;
+    public GameObject premiumPassTier57;
+    public GameObject premiumPassTier58;
+    public GameObject premiumPassTier59;
+    public GameObject premiumPassTier60;
+    public GameObject premiumPassTier61;
+    public GameObject premiumPassTier62;
+    public GameObject premiumPassTier63;
+    public GameObject premiumPassTier64;
+    public GameObject premiumPassTier65;
+    public GameObject premiumPassTier66;
+    public GameObject premiumPassTier67;
+    public GameObject premiumPassTier68;
+    public GameObject premiumPassTier69;
+    public GameObject premiumPassTier70;
+    public GameObject premiumPassTier71;
+    public GameObject premiumPassTier72;
+    public GameObject premiumPassTier73;
+    public GameObject premiumPassTier74;
+    public GameObject premiumPassTier75;
+    public GameObject premiumPassTier76;
+    public GameObject premiumPassTier77;
+    public GameObject premiumPassTier78;
+    public GameObject premiumPassTier79;
+    public GameObject premiumPassTier80;
+    public GameObject premiumPassTier81;
+    public GameObject premiumPassTier82;
+    public GameObject premiumPassTier83;
+    public GameObject premiumPassTier84;
+    public GameObject premiumPassTier85;
+    public GameObject premiumPassTier86;
+    public GameObject premiumPassTier87;
+    public GameObject premiumPassTier88;
+    public GameObject premiumPassTier89;
+    public GameObject premiumPassTier90;
+    
+    
+    public void Start()
+    {
+       premiumPassActivationValue = 0;
+       freePassCurrentTier = 1;
+       premiumPassCurrentTier = 1;
+       
+       freePassTierCap = 100;
+       premiumPassTierCap = 100;
+    }
+    
+    public void Update()
+    {
+      
+    }
+}
